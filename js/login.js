1d570d9601be4f59e2ea8a335b3c0a28dd3f301e
NET24BD <284401873+NET24BD@users.noreply.github.com>
2026-07-08 23:48:06 +0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
-// MY AI JS
+function login(){
+
+    const user=document.getElementById("username").value.trim();
+
+    const pass=document.getElementById("password").value.trim();
+
+    if(user==="" || pass===""){
+
+        alert("Please enter Username & Password");
+
+        return;
+
+    }
+
+    // এখন শুধু ডেমো
+    window.location.href="chat.html";
+
+}
